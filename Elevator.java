0aca4ab0291d9bc306ff3ad13953433df0f1c8e1
socratest3 <socratest3@nycstudents.net>
2025-05-23 12:19:25 -0400

    Update Elevator.java

--- Elevator.java
+++ Elevator.java
@@ -1,1 +1,33 @@
+public class Elevator {
+
+private int weightLimit = 2000;
+private boolean inUse;
+private int currentWeight;
+
+private boolean elevatorPass;
+
+public Elevator(int currentWeight, boolean inUse) {
+    this.currentWeight = currentWeight;
+    this.inUse = inUse;
+}
+
+public void changePass(boolean equalsTrue) {
+     elevatorPass = equalsTrue;
+}
+
+public int getWeightLimit() {
+    return weightLimit;
+}
+
+public int getCurrentWeight() {
+    return currentWeight;
+}
+
+public String displayStatus(String elevator) {
+    
+}
+}
+
+   
+
 
